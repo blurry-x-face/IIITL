0df5de9ce547d8f11ce9d7dc640f2e14f0711718
Firela <rishabhshukla1999@gmail.com>
2019-04-23 12:53:36 +0530

    changed to hash router

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -33,7 +33,7 @@ class Carousel extends React.Component {
     // for (var i = 0; i < 200; i++) {
     let l = 0;
     l = setInterval(() => {
-      if (window.location.pathname === "/") {
+      if (window.location.hash === "#/") {
         var slide = document.querySelectorAll(".block");
         var j = this.state.slideNo;
         slide[j].classList.remove("active");
