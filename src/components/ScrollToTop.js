c4f3a8f13c5f8ed447c4cad3057082daf5dce687
Firela <rishabhshukla1999@gmail.com>
2019-04-25 18:08:14 +0530

    add smooth scroll

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,7 +1,11 @@
 import React from "react";
 class ScrollToTopOnMount extends React.Component {
   componentDidMount() {
-    window.scrollTo(0, 0);
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
   }
 
   render() {
